c3c56373aedcc3b0f94b736a7108160fb57ddea2
MichaelLaguna <I202404641@cibertec.edu.pe>
2026-02-22 16:55:12 -0500

    Configurar Navigation Component y BottomNavigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("org.osmdroid:osmdroid-android:6.1.18")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
 
 
